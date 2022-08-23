fe016e92250e2fce397b15bb9180b5cab0d5379d
Florian Walter <nairolf.retlaw@gmail.com>
2022-08-23 09:50:32 +0200

    fix telescope folding and preview for large files

--- dot_config/nvim/lua/configs/telescope.lua
+++ dot_config/nvim/lua/configs/telescope.lua
@@ -9,74 +9,46 @@ function M.config()
   local actions = require 'telescope.actions'
   telescope.load_extension 'fzf'
 
+  vim.api.nvim_create_autocmd('BufRead', {
+    callback = function()
+      vim.api.nvim_create_autocmd('BufWinEnter', {
+        once = true,
+        command = 'normal! zx'
+      })
+    end
+  })
+
+  local previewers = require('telescope.previewers')
+  local previewers_utils = require('telescope.previewers.utils')
+  local max_size = 100000
+
+  local function preview_maker(filepath, bufnr, opts)
+    opts = opts or {}
+
+    filepath = vim.fn.expand(filepath)
+    vim.loop.fs_stat(filepath, function(_, stat)
+      if not stat then
+        return
+      end
+
+      if stat.size > max_size then
+        local cmd = { "head", "-c", max_size, filepath }
+        previewers_utils.job_maker(cmd, bufnr, opts)
+      else
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      end
+    end)
+  end
+
   telescope.setup {
     defaults = {
       prompt_prefix = ' ',
       selection_caret = '❯ ',
       path_display = { 'smart' },
-      file_ignore_patterns = { 'third_party', '^/usr/include' }
+      file_ignore_patterns = { 'third_party', '^/usr/include' },
 
-    --   mappings = {
-    --     i = {
-    --       ['<C-n>'] = actions.cycle_history_next,
-    --       ['<C-p>'] = actions.cycle_history_prev,
-
-    --       ['<C-j>'] = actions.move_selection_next,
-    --       ['<C-k>'] = actions.move_selection_previous,
-
-    --       ['<C-c>'] = actions.close,
-
-    --       ['<Down>'] = actions.move_selection_next,
-    --       ['<Up>'] = actions.move_selection_previous,
-
-    --       ['<CR>'] = actions.select_default,
-    --       ['<C-x>'] = actions.select_horizontal,
-    --       ['<C-v>'] = actions.select_vertical,
-    --       ['<C-t>'] = actions.select_tab,
-
-    --       ['<C-u>'] = actions.preview_scrolling_up,
-    --       ['<C-d>'] = actions.preview_scrolling_down,
-
-    --       ['<PageUp>'] = actions.results_scrolling_up,
-    --       ['<PageDown>'] = actions.results_scrolling_down,
-
-    --       ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
-    --       ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
-    --       ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
-    --       ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-    --       ['<C-l>'] = actions.complete_tag,
-    --     },
-
-    --     n = {
-    --       ['<esc>'] = actions.close,
-    --       ['<CR>'] = actions.select_default,
-    --       ['<C-x>'] = actions.select_horizontal,
-    --       ['<C-v>'] = actions.select_vertical,
-    --       ['<C-t>'] = actions.select_tab,
-
-    --       ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
-    --       ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
-    --       ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
-    --       ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-
-    --       ['j'] = actions.move_selection_next,
-    --       ['k'] = actions.move_selection_previous,
-    --       ['H'] = actions.move_to_top,
-    --       ['M'] = actions.move_to_middle,
-    --       ['L'] = actions.move_to_bottom,
-
-    --       ['<Down>'] = actions.move_selection_next,
-    --       ['<Up>'] = actions.move_selection_previous,
-    --       ['gg'] = actions.move_to_top,
-    --       ['G'] = actions.move_to_bottom,
-
-    --       ['<C-u>'] = actions.preview_scrolling_up,
-    --       ['<C-d>'] = actions.preview_scrolling_down,
-
-    --       ['<PageUp>'] = actions.results_scrolling_up,
-    --       ['<PageDown>'] = actions.results_scrolling_down,
-    --     },
-    --   },
+      vimgrep_arguments = { 'ag', '--vimgrep' },
+      buffer_previewer_maker = preview_maker,
     },
     pickers = {},
     extensions = {
